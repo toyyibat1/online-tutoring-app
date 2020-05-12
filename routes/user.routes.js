1b34249f1a28c61b79b760bb0cd61f1c5687c342
unknown <toyyibat.abdulkareem@gmail.com>
2020-05-12 23:46:34 +0100

    delete some folder

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { authJwt } = require("../middleware/auth");
+const authJwt  = require("../middleware/auth");
 const controller = require("../controllers/user.contoller");
 
-router.get('/category/:name', controller.getsubjectByCategory);
-router.get('/categories', controller.findAllCategories);
-router.get('/category/subjectId', controller.getSubjectById);
-router.get('/subjectName/:name', controller.searchSubjectByName)
-router.get('/tutorName/:name', controller.searchTutorByName)
+router.get('/category/:name', authJwt.verifyToken, controller.getsubjectByCategory);
+router.get('/categories', authJwt.verifyToken, controller.findAllCategories);
+router.get('/category/subjectId', authJwt.verifyToken, controller.getSubjectById);
+router.get('/subjectName/:name', authJwt.verifyToken,controller.searchSubjectByName)
+router.get('/tutorName/:name', authJwt.verifyToken, controller.searchTutorByName)
 
 module.exports = router;
